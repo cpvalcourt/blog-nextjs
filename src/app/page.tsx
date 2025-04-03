50f93576a01744a564afb9510c09648783f5b0fb
Christopher Valcourt <valcourc@bc.edu>
2025-04-03 13:06:27 -0400

    another round of style updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="container mx-auto p-4 relative">
-        <div className="bg-white border-2 border-green-100 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white border-2 border-emerald-500 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
             <h1 className={`text-4xlfont-bold mb-4 ${styles.text_green}`}>
-              Welcome
+              Welcome to my Blog
             </h1>
             <p className="text-lg text-gray-700 mb-4">
               Massa urna magnis dignissim id euismod porttitor vitae etiam
@@ -17,7 +17,7 @@ export default function Home() {
             </p>
             <a
               href="/blog/posts"
-              className={`outline outline-1 outline-offset-2 border-green-500 text-green-500 hover:text-white py-2 px-4 rounded hover:bg-green-500 md:w-auto ${styles.fit_content}`}
+              className={`outline outline-1 outline-offset-2 border-emerald-500 text-emerald-500 hover:text-white py-2 px-4 rounded hover:bg-emerald-500 md:w-auto ${styles.fit_content}`}
             >
               Go to Blog
             </a>
